0725a7f53107263d7bc3263f4d71f3b29a21a004
dbriksza <dbriksza@gmail.com>
2020-05-14 19:15:05 -0400

    added logo buttons, added new project to project page

--- portfolio/src/components/projects.js
+++ portfolio/src/components/projects.js
@@ -12,10 +12,10 @@ const Projects = () => {
           in 8 weeks. The team that created this consisted of myself and 5 other
           full stack web developers, 3 data scientists, and 1 UX designer. The
           data is compiled mostly from US census data and Zillow, but with a
-          couple smaller api's for more niche data (walk score, transit score,
+          couple smaller API's for more niche data (walk score, transit score,
           weather, etc.) The main features I worked on were: backend user
-          functionality (OAuth, RDBMS(SQLite for development and Postgres for
-          production)), front end animations (css animations, minor graphic
+          functionality (OAuth, RDBMS (SQLite for development and Postgres for
+          production)), front end animations (CSS animations, minor graphic
           design work) and map features (rendering data points and on-hover city
           view).
         </p>
@@ -32,6 +32,36 @@ const Projects = () => {
         </ul>
       </div>
       <iframe className="iframe-loading" src="https://v1.citrics.io">
+        > Sorry your browser does not support inline frames.
+      </iframe>
+      <div>
+        <h3>
+          <a href="https://frontend.bw-secret-family-recipes.now.sh">
+            Secret Family Recipe
+          </a>
+        </h3>
+        <p class="projectBlurbs">
+          Secret Family Recipes, while originally intended to be an online index
+          of privately viewable user-generated recipes, ended up being an
+          interface for saving recipes locally (The backend developer fell
+          through, but being agile devs, we were able to salvage the project).
+          Built in a week by 5 front-end developers, this website highlights the
+          ability to work quickly and change direction at a moment's notice.
+          This was all done in order to meet deadlines and push out a product
+          that meets and exceeds the minimum viable product
+        </p>
+        <h4>Tech Stack</h4>
+        <ul>
+          <li>React</li>
+          <li>Sass</li>
+          <li>Styled Compnents</li>
+          <li>Redux</li>
+        </ul>
+      </div>
+      <iframe
+        className="iframe-loading"
+        src="https://frontend.bw-secret-family-recipes.now.sh"
+      >
         > Sorry your browser does not support inline frames.
       </iframe>
     </div>
